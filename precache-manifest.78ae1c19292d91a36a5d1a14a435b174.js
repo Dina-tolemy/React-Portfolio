2d20e716d660b6bfcb1e9b3c32dcfff833da295f
Dinatolemy <dina.a.tolemy@gmail.com>
2020-04-14 19:07:10 +1000

    Updates

---
+++ precache-manifest.78ae1c19292d91a36a5d1a14a435b174.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d4ac05a8e632f38fda4113db49c0af",
+    "url": "/DeployedReact-Portfolio/index.html"
+  },
+  {
+    "revision": "89a6c50f4aed2894296d",
+    "url": "/DeployedReact-Portfolio/static/css/main.c239f8e2.chunk.css"
+  },
+  {
+    "revision": "81322b78334fd980ded2",
+    "url": "/DeployedReact-Portfolio/static/js/2.6fe7fba7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DeployedReact-Portfolio/static/js/2.6fe7fba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a6c50f4aed2894296d",
+    "url": "/DeployedReact-Portfolio/static/js/main.331ee655.chunk.js"
+  },
+  {
+    "revision": "eb1f3a269d699e764b1f",
+    "url": "/DeployedReact-Portfolio/static/js/runtime-main.607d9d78.js"
+  },
+  {
+    "revision": "e8925c71b4fa5703ed2c7111f0a184cf",
+    "url": "/DeployedReact-Portfolio/static/media/background.e8925c71.jpg"
+  },
+  {
+    "revision": "35356d92d8e9ccc67d53fc705f76d5fb",
+    "url": "/DeployedReact-Portfolio/static/media/profile8.35356d92.jpg"
+  }
+]);
